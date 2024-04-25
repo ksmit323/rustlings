48d35a919344ecb59d97aa2157803c92ab0b56a1
Kenneth Smith <ksmit323@gmail.com>
2024-04-25 12:09:34 +0700

    add solutions

--- exercises/06_move_semantics/move_semantics4.rs
+++ exercises/06_move_semantics/move_semantics4.rs
@@ -7,20 +7,26 @@
 // Execute `rustlings hint move_semantics4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 #[test]
 fn main() {
+<<<<<<< HEAD:exercises/06_move_semantics/move_semantics4.rs
     let vec0 = vec![22, 44, 66];
 
     let vec1 = fill_vec(vec0);
 
+=======
+    
+    let mut vec1 = fill_vec();
+    
+>>>>>>> new-branch:exercises/move_semantics/move_semantics4.rs
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument - don't change this!
 fn fill_vec() -> Vec<i32> {
     // Instead, let's create and fill the Vec in here - how do you do that?
+    let vec = vec![22, 44, 66];
     let mut vec = vec;
 
     vec.push(88);
--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,25 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    String::from(input.trim_start().trim_end())
 }
 
 fn compose_me(input: &str) -> String {
+<<<<<<< HEAD:exercises/09_strings/strings3.rs
     // TODO: Add " world!" to the string! There are multiple ways to do this!
     ???
+=======
+    // TODO: Add " world!" to the string! There's multiple ways to do this!
+    String::from(input) + " world!"
+>>>>>>> new-branch:exercises/strings/strings3.rs
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
